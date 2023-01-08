d69b40a17fb1958692950fcf6952e2ad86bcc4a6
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2023-01-07 23:17:27 -0700

    Add files via upload

---
+++ lua/entities/sent_onoffswitch.lua
@@ -0,0 +1,195 @@
+AddCSLuaFile()
+
+-- set up the On/Off Switch SENT
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+
+-- give the On/Off Switch a name and model
+ENT.PrintName = "On/Off Switch"
+ENT.Model = "models/onoffswitch.mdl"
+
+-- other junk lmaoooo
+ENT.Editable 			=	true
+ENT.Spawnable			=	true
+ENT.AdminOnly			=	false
+ENT.Category 			=	"Super Garry Maker 2"
+ENT.Author				= 	"Dogeiscut"
+
+ENT.IconOverride		=	"materials/entities/onoffswitch.jpg"
+
+-- set up the On/Off Switch's default state
+ENT.Channel = 0
+OnOffState = {}
+OnOffState[ENT.Channel] = true
+
+ENT.DataTables = { "Channel" }
+
+ENT.HitCooldown = CurTime()
+
+-- define a table of colors for each channel
+ENT.CHANNEL_MATERIALS = {
+	[0] = {
+		on = "models/onoffswitch_on.vmt",
+		off = "models/onoffswitch_off.vmt"
+	},
+}
+
+sound.Add( {
+	name = "onoffswitch_switch",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 80,
+	pitch = {100, 100},
+	sound = "switch.wav"
+} )
+
+function ENT:SetupDataTables()
+
+	self:NetworkVar( "Bool", 0, "State", { KeyName = "state", Edit = { type = "Bool", order = 1 } } )
+	
+end
+
+function ENT:Initialize()
+
+	if ( CLIENT ) then return end
+	
+	self:SetModel("models/onoffswitch.mdl")
+	self:SetSolid(SOLID_VPHYSICS)
+	self:SetMoveType( MOVETYPE_VPHYSICS )
+	self:PhysicsInit( SOLID_VPHYSICS )
+
+	self:SetPlaybackRate( 2 )
+	self:ResetSequenceInfo()
+	self:SetSequence("Bump")
+
+	if OnOffState[self.Channel] == true then
+		self:TurnOn(false)
+	else
+		self:TurnOff(false)
+	end
+	self:SnapToGrid()
+end
+
+-- set up the On/Off Switch's interaction functions
+function ENT:Touch(ent)
+	if ent:IsPlayer() and ent:GetVelocity().z > 0 and ent:EyePos().z < self:GetPos().z then
+		if OnOffState[self.Channel] then
+			self:TurnOff(true)
+		else
+			self:TurnOn(true)
+		end
+	end
+end
+
+function ENT:OnTakeDamage(dmg)
+	if dmg:GetDamage() >= 0 then
+		if OnOffState[self.Channel] then
+			self:TurnOff(true)
+		else
+			self:TurnOn(true)
+		end
+	end
+end
+
+-- set up the On/Off Switch's state change functions
+function ENT:TurnOn(itself)
+
+	self:RemoveAllDecals()
+	if itself and ((CurTime()-self.HitCooldown)<0.25) then return end
+
+	self:SetMaterial(self.CHANNEL_MATERIALS[self.Channel].on)
+
+	if itself then
+
+		self.HitCooldown = CurTime()
+
+		-- if not self:IsSequenceFinished() then return end
+
+		self:ResetSequenceInfo()
+		self:SetPlaybackRate( 2 )
+		self:SetSequence("Bump")
+		self:EmitSound("onoffswitch_switch")
+		OnOffState[self.Channel] = true
+	
+		for i, ent in ipairs( ents.FindByClass( "sent_onoffswitch" ) ) do
+			ent:TurnOn(false)
+		end
+		for i, ent in ipairs( ents.FindByClass( "sent_reddottedlineblock" ) ) do
+			ent:TurnOn(false)
+		end
+		for i, ent in ipairs( ents.FindByClass( "sent_bluedottedlineblock" ) ) do
+			ent:TurnOn(false)
+		end
+	end
+end
+
+function ENT:TurnOff(itself)
+
+	self:RemoveAllDecals()
+	if itself and ((CurTime()-self.HitCooldown)<0.25) then return end
+	
+	self:SetMaterial(self.CHANNEL_MATERIALS[self.Channel].off)
+
+	if itself then
+
+		self.HitCooldown = CurTime()
+
+		-- if not self:IsSequenceFinished() then return end
+
+		self:ResetSequenceInfo()
+		self:SetPlaybackRate( 2 )
+		self:SetSequence("Bump")
+		self:EmitSound("onoffswitch_switch")
+		OnOffState[self.Channel] = false
+
+		for i, ent in ipairs( ents.FindByClass( "sent_onoffswitch" ) ) do
+			ent:TurnOff(false)
+		end
+		for i, ent in ipairs( ents.FindByClass( "sent_reddottedlineblock" ) ) do
+			ent:TurnOff(false)
+		end
+		for i, ent in ipairs( ents.FindByClass( "sent_bluedottedlineblock" ) ) do
+			ent:TurnOff(false)
+		end
+	end
+end
+
+function ENT:SnapToGrid() 
+	-- get the model size of the On/Off Block
+	local modelSize = self:GetModelBounds()
+	
+	-- calculate the grid size based on the model size
+	local gridSize = 32 -- math.max(modelSize.x, modelSize.y, modelSize.z)*2
+	
+	-- snap the position of the On/Off Block to the grid
+	local pos = self:GetPos()
+	pos.x = math.Round(pos.x / gridSize) * gridSize
+	pos.y = math.Round(pos.y / gridSize) * gridSize
+	pos.z = math.Round(pos.z / gridSize) * gridSize
+	self:SetPos(pos)
+	
+	-- snap the angle of the On/Off Block to a multiple of 90 degrees
+	local ang = self:GetAngles()
+	ang.pitch = math.Round(ang.pitch / 90) * 90
+	ang.yaw = math.Round(ang.yaw / 90) * 90
+	ang.roll = math.Round(ang.roll / 90) * 90
+	self:SetAngles(ang)
+
+	self:GetPhysicsObject():EnableMotion( false )
+end
+
+hook.Add("PhysgunDrop", "OnoffswitchStop", function(ply, ent)
+	if ent:GetClass()=="sent_onoffswitch" then
+		ent:SnapToGrid()
+	end
+end )
+
+if ( SERVER ) then return end
+
+function ENT:Draw()
+	self:DrawModel()
+	self:DrawShadow(true)	
+end
+
+function ENT:Think()
+end
---
+++ lua/weapons/swep_mariosshoes/shared.lua
@@ -0,0 +1,120 @@
+if SERVER then
+    AddCSLuaFile()
+  end
+  
+  SWEP.PrintName = "Mario's Shoes"
+  
+  SWEP.Slot = 1
+  SWEP.SlotPos = 1
+  
+  SWEP.DrawAmmo = false
+  SWEP.DrawCrosshair = true
+  
+  SWEP.Author = "DogeisCut"
+  SWEP.Instructions = "Tap to do a small jump or hold it for a big jump! Overrides fall damage and makes you slightly smaller."
+  
+  SWEP.Spawnable = true
+  SWEP.AdminOnly = false
+  
+  SWEP.ViewModel = "models/hunter/blocks/cube8x8x8.mdl"
+  SWEP.WorldModel = "models/props_junk/Shoe001a.mdl"
+  SWEP.Category =	"Super Garry Maker 2"
+  SWEP.UseHands = true
+
+  SWEP.IconOverride = "entities/mariosboots.jpg"
+  
+  SWEP.Primary.ClipSize = -1
+  SWEP.Primary.DefaultClip = -1
+  SWEP.Primary.Automatic = false
+  SWEP.Primary.Ammo = "none"
+  
+  SWEP.Secondary.ClipSize = -1
+  SWEP.Secondary.DefaultClip = -1
+  SWEP.Secondary.Automatic = false
+  SWEP.Secondary.Ammo = "none"
+  
+  SWEP.OwnerGravityBeforeUpdate = 1
+  
+  sound.Add( {
+    name = "mario_jump",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 80,
+    pitch = {100, 100},
+    sound = "jump.wav"
+  } )
+
+  function SWEP:Initialize()
+
+    self:SetHoldType("normal")
+  end
+
+  function SWEP:Initialize()
+    self:SetHoldType("normal")
+    
+  end
+  
+  function SWEP:Deploy()
+    local ply = self:GetOwner()
+    local bottom, top = ply:GetHull()
+
+    self.OwnerGravityBeforeUpdate = ply:GetGravity()
+    
+    ply:SetJumpPower(ply:GetJumpPower() * 1.2)
+    ply:SetMaxSpeed(ply:GetMaxSpeed() * 1.2)
+    ply:SetRunSpeed(ply:GetRunSpeed() * 1.2)
+    ply:SetWalkSpeed(ply:GetWalkSpeed() * 1.2)
+    ply:SetHull( bottom * 0.9, top * 0.9 )
+
+    hook.Add("GetFallDamage", ply, function(ply, speed)
+      if ply == self:GetOwner() then
+        return 0
+      end
+    end)
+    
+    return true
+  end
+
+  function SWEP:Holster()
+    if SERVER then
+      local ply = self:GetOwner()
+      local bottom, top = ply:GetHull()
+
+      
+      ply:SetGravity(self.OwnerGravityBeforeUpdate*1)
+      
+      ply:SetJumpPower(ply:GetJumpPower() / 1.2)
+      ply:SetMaxSpeed(ply:GetMaxSpeed() / 1.2)
+      ply:SetRunSpeed(ply:GetRunSpeed() / 1.2)
+      ply:SetWalkSpeed(ply:GetWalkSpeed() / 1.2)
+      ply:SetHull( bottom / 0.9, top / 0.9  )
+
+      hook.Remove("GetFallDamage", ply)
+      
+      return true
+    end
+  end
+
+  function SWEP:PrimaryAttack()
+    return false
+  end
+
+  function SWEP:SecondaryAttack()
+    return false
+  end
+
+  function SWEP:Think()
+    if SERVER then
+      local ply = self:GetOwner()
+      if ply:KeyDown(IN_JUMP) and ply:GetVelocity().z>0 then
+        if not (ply:GetGravity() == self.OwnerGravityBeforeUpdate*0.25) then
+          ply:EmitSound("mario_jump")
+        end
+        ply:SetGravity(self.OwnerGravityBeforeUpdate*0.25)
+        
+      end
+      if ply:KeyReleased(IN_JUMP) or (ply:KeyDown(IN_JUMP) and not (ply:GetVelocity().z>0)) then
+        ply:SetGravity(self.OwnerGravityBeforeUpdate*1)
+      end
+    end
+  end
